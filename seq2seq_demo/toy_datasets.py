66dbf9b69b2945989ed31dab73ea4672dd148b90
JJJJerry <1171215015@qq.com>
2024-12-10 15:44:15 +0800

    RNN和LSTM DEMO的更新

--- seq2seq_demo/toy_datasets.py
+++ seq2seq_demo/toy_datasets.py
@@ -27,3 +27,29 @@ def get_dataset_add1(data_num=2000,vocab_size=16,seq_len=8,batch_first=False):
             }
         return batch
     return dataset,collate_fn
+
+def get_dataset_repeat(data_num=2000,vocab_size=16,seq_len=8,batch_first=False):
+    """
+        生成一个重复数字的数据集，输入是一个长度为seq_len的随机序列，输出序列和输入序列一样的结果。
+    """
+    dataset=[]
+    for _ in range(data_num):
+        src=torch.tensor([random.randint(0,vocab_size-2) for i in range(seq_len)])
+        tgt=src
+        dataset.append(
+            {
+                'src':src,
+                'tgt':tgt
+            }
+        )
+    def collate_fn(batch):
+        src=[b['src'] for b in batch]
+        tgt=[b['tgt'] for b in batch]
+        src=nn.utils.rnn.pad_sequence(src,batch_first=batch_first)
+        tgt=nn.utils.rnn.pad_sequence(tgt,batch_first=batch_first)
+        batch={
+                'src':src,
+                'tgt':tgt
+            }
+        return batch
+    return dataset,collate_fn
